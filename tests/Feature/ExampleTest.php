e4359119d8fdd2dbcae795587c5b51388b98a53a
GitHub Action <action@github.com>
2022-03-01 12:37:32 +0000

    Update code-style 💻

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,7 +3,6 @@
 namespace JagdishJP\SBIPay\Tests\Feature;
 
 use JagdishJP\SBIPay\Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class ExampleTest extends TestCase
 {
@@ -15,7 +14,7 @@ class ExampleTest extends TestCase
     public function testTheApplicationReturnsASuccessfulResponse()
     {
         $this->withoutMiddleware();
-        
+
         $response = $this->get('/');
 
         $response->assertStatus(200);
