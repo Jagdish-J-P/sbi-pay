f0cc55002e48302a29fe1366848a5a56d2b9dc65
GitHub Action <action@github.com>
2022-05-27 05:26:38 +0000

    Update code-style 💻

--- src/Console/Commands/SbiPayPublish.php
+++ src/Console/Commands/SbiPayPublish.php
@@ -3,63 +3,66 @@
 namespace JagdishJP\SBIPay\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Str;
 
-class SbiPayPublish extends Command {
+class SbiPayPublish extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sbi-pay:publish {--force : override existing files.}';
 
-	/**
-	 * The name and signature of the console command.
-	 *
-	 * @var string
-	 */
-	protected $signature = 'sbi-pay:publish {--force : override existing files.}';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Publishes SB-Pay publishable resources.';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Publishes SB-Pay publishable resources.';
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct() {
-		parent::__construct();
-	}
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $publishables = ['config', 'controller', 'assets', 'views'];
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return int
-	 */
-	public function handle() {
-		$publishables = ['config', 'controller', 'assets', 'views'];
+        $force = $this->argument('force');
 
-		$force = $this->argument('force');
+        foreach ($publishables as $publishable) {
+            if (! empty($force) && ! Str::is($force, 'force')) {
+                $this->error('Invalid Argument. syntax: php artisan fpx:publish force');
 
-		foreach ($publishables as $publishable) {
+                return 0;
+            }
 
-			if (!empty($force) && !Str::is($force, 'force')) {
-				$this->error("Invalid Argument. syntax: php artisan fpx:publish force");
-				return 0;
-			}
+            $parameters = ['--provider' => 'JagdishJP\SBIPay\SBIPayServiceProvider', '--tag' => "sbi-pay-{$publishable}"];
 
-			$parameters = ['--provider' => 'JagdishJP\SBIPay\SBIPayServiceProvider', '--tag' => "sbi-pay-{$publishable}"];
+            if (Str::is($force, 'force')) {
+                $parameters['--force'] = null;
+            }
 
-			if (Str::is($force, 'force'))
-				$parameters['--force'] = null;
+            $this->info("Publishing {$publishable} file.");
 
-			$this->info("Publishing {$publishable} file.");
+            Artisan::call('vendor:publish', $parameters);
+        }
 
-			Artisan::call("vendor:publish", $parameters);
-		}
+        Artisan::call('config:cache');
 
-		Artisan::call("config:cache");
-
-		$this->info('Publishing completed.');
-	}
+        $this->info('Publishing completed.');
+    }
 }
